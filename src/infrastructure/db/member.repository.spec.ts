415ee2946cdee3ee8d1005f6c4bca9cf8f9e2dff
maaaaaaaaad <integral5137@gmail.com>
2024-03-07 21:19:57 +0900

    e

--- src/infrastructure/db/member.repository.spec.ts
+++ src/infrastructure/db/member.repository.spec.ts
@@ -1,5 +1,6 @@
 import { PrismaService } from '@/infrastructure/services/prisma.service';
 import { MemberRepository } from '@/infrastructure/db/member.repository';
+import { Member } from '@/domain/entities/member/member';
 
 jest.mock('@/infrastructure/services/prisma.service', () => ({
     PrismaService: jest.fn().mockImplementation(() => ({
@@ -19,7 +20,46 @@ describe('member repository', () => {
         repository = new MemberRepository(mockPrismaService);
     });
     describe('insert', () => {
-        it('success', async () => {});
+        const id = 'mock_uuid';
+        const email = 'mock@gmail.com';
+        const password = 'mock_password';
+        const cash = 0;
+        const created_at = new Date();
+        const updated_at = new Date();
+        const deleted_at = null;
+        it('success', async () => {
+            const transaction = jest
+                .mocked(mockPrismaService.$transaction)
+                .mockImplementation(async (transactionCallback) =>
+                    transactionCallback(mockPrismaService),
+                );
+            const create = jest
+                .mocked(mockPrismaService.members.create)
+                .mockResolvedValueOnce({
+                    id,
+                    email,
+                    cash,
+                    password,
+                    created_at,
+                    updated_at,
+                    deleted_at,
+                });
+            const result = await repository.insert(email, password);
+            const member = new Member(
+                id,
+                email,
+                password,
+                cash,
+                created_at,
+                updated_at,
+                deleted_at,
+            );
+            expect(result).toMatchObject(member);
+            expect(transaction).toHaveBeenCalled();
+            expect(transaction).toBeCalledTimes(1);
+            expect(create).toHaveBeenCalled();
+            expect(create).toBeCalledTimes(1);
+        });
         it('should failed because 422 email', async () => {});
     });
 });
