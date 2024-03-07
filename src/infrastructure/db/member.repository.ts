358c5df2b7ce32d9cc00b50a42b47e19dbd4822c
maaaaaaaaad <integral5137@gmail.com>
2024-03-07 20:02:21 +0900

    e

--- src/infrastructure/db/member.repository.ts
+++ src/infrastructure/db/member.repository.ts
@@ -6,19 +6,19 @@ import { PrismaService } from '@/infrastructure/services/prisma.service';
 @Injectable({ scope: Scope.DEFAULT })
 export class MemberRepository implements MemberRepositoryInterface {
     constructor(private readonly prisma: PrismaService) {}
-    public deleteSoftById<K>(id: string): Promise<K> {
+    public async deleteSoftById<K>(id: string): Promise<K> {
         return Promise.resolve(undefined);
     }
 
-    public findOneById(id: string): Promise<Member> {
+    public async findOneById(id: string): Promise<Member> {
         return Promise.resolve(undefined);
     }
 
-    public insert(email: string, password: string): Promise<Member> {
+    public async insert(email: string, password: string): Promise<Member> {
         return Promise.resolve(undefined);
     }
 
-    public update<T>(id: string, data: Partial<T>): Promise<Member> {
+    public async update<T>(id: string, data: Partial<T>): Promise<Member> {
         return Promise.resolve(undefined);
     }
 }
